bb5c00b5d8804ed96b4749ed7a3a523edfaf94b0
Ning Sun <sunng@protonmail.com>
2022-06-29 22:36:49 +0800

    feat: add more built-in functionality to startuphandler

--- examples/server.rs
+++ examples/server.rs
@@ -1,59 +1,63 @@
-#[macro_use]
-extern crate tokio;
-
+use std::collections::HashMap;
 use std::fmt::Debug;
 
 use async_trait::async_trait;
 use futures::{Sink, SinkExt};
-use futures::stream;
 use tokio::net::{TcpListener, TcpStream};
+use tokio_stream::StreamExt;
 use tokio_util::codec::Framed;
-use tokio_stream:: StreamExt;
-use rand;
 
-use pgwire::api::{ClientInfo, ClientInfoHolder};
-use pgwire::messages::PgWireMessage;
-use pgwire::messages::startup::{Authentication, ParameterStatus, BackendKeyData};
-use pgwire::messages::response::{READY_STATUS_IDLE, ReadyForQuery};
-use pgwire::tokio::PgWireMessageServerCodec;
+use pgwire::api::auth::StartupHandler;
 use pgwire::api::PgWireConnectionState;
-use pgwire::api::auth::Authenticator;
+use pgwire::api::{ClientInfo, ClientInfoHolder};
+use pgwire::messages::startup::Authentication;
+use pgwire::messages::PgWireMessage;
+use pgwire::tokio::PgWireMessageServerCodec;
 
 pub struct DummyAuthenticator;
 
 #[async_trait]
-impl Authenticator for DummyAuthenticator {
+impl StartupHandler for DummyAuthenticator {
     async fn on_startup<C>(
         &self,
         client: &mut C,
         message: &PgWireMessage,
-    ) -> Result<(), std::io::Error> where C: ClientInfo + Sink<PgWireMessage> + Unpin + Send, C::Error: Debug {
+    ) -> Result<(), std::io::Error>
+    where
+        C: ClientInfo + Sink<PgWireMessage> + Unpin + Send,
+        C::Error: Debug,
+    {
         println!("{:?}, {:?}", client.socket_addr(), message);
         match message {
             PgWireMessage::Startup(ref startup) => {
                 // TODO: update metadata with startup parameters
+                self.handle_startup_parameters(client, startup);
                 client.set_state(PgWireConnectionState::AuthenticationInProgress);
-                client.send(PgWireMessage::Authentication(Authentication::CleartextPassword)).await.unwrap();
-            },
-            PgWireMessage::Password(ref password) => {
-                client.set_state(PgWireConnectionState::ReadyForQuery);
-
-                // TODO: auto generate these responses
-                let messages = vec![PgWireMessage::Authentication(Authentication::Ok),
-                                    PgWireMessage::ParameterStatus(ParameterStatus::new("application_name".into(), "psql".into())),
-                                    PgWireMessage::ParameterStatus(ParameterStatus::new("integer_datetimes".into(), "on".into())),
-                                    PgWireMessage::BackendKeyData(BackendKeyData::new(std::process::id() as i32, rand::random::<i32>())),
-                                    PgWireMessage::ReadyForQuery(ReadyForQuery::new(READY_STATUS_IDLE))
-                        ];
-                let mut message_stream = stream::iter(messages.into_iter().map(Ok));
-                client.send_all(&mut message_stream).await.unwrap();
-            },
+                client
+                    .send(PgWireMessage::Authentication(
+                        Authentication::CleartextPassword,
+                    ))
+                    .await
+                    .unwrap();
+            }
+            PgWireMessage::Password(ref _password) => self.finish_authentication(client).await,
             _ => {}
         }
         Ok(())
     }
-}
 
+    fn server_parameters<C>(&self, _client: &C) -> std::collections::HashMap<String, String>
+    where
+        C: ClientInfo + Sink<PgWireMessage> + Unpin + Send,
+        C::Error: Debug,
+    {
+        let mut data = HashMap::new();
+        data.insert("application_name".into(), "psql".into());
+        data.insert("integer_datetimes".into(), "on".into());
+
+        data
+    }
+}
 
 #[tokio::main]
 pub async fn main() {
@@ -71,8 +75,12 @@ pub async fn main() {
     }
 }
 
-
-async fn process_socket<A>(mut socket: Framed<TcpStream, PgWireMessageServerCodec>, authenticator: A) where A: Authenticator {
+async fn process_socket<A>(
+    mut socket: Framed<TcpStream, PgWireMessageServerCodec>,
+    authenticator: A,
+) where
+    A: StartupHandler,
+{
     // client ssl request, return
     loop {
         match socket.next().await {
@@ -81,24 +89,28 @@ async fn process_socket<A>(mut socket: Framed<TcpStream, PgWireMessageServerCode
                 match socket.codec().client_info().state() {
                     PgWireConnectionState::AwaitingSslRequest => {
                         if matches!(msg, PgWireMessage::SslRequest(_)) {
-                            socket.codec_mut().client_info_mut().set_state(PgWireConnectionState::AwaitingStartup);
+                            socket
+                                .codec_mut()
+                                .client_info_mut()
+                                .set_state(PgWireConnectionState::AwaitingStartup);
                             socket.send(PgWireMessage::SslResponse(b'N')).await.unwrap();
                         } else {
                             // TODO: raise error here for invalid packet read
                             unreachable!()
                         }
-                    },
-                    PgWireConnectionState::AwaitingStartup | PgWireConnectionState::AuthenticationInProgress => {
+                    }
+                    PgWireConnectionState::AwaitingStartup
+                    | PgWireConnectionState::AuthenticationInProgress => {
                         authenticator.on_startup(&mut socket, &msg).await.unwrap();
-                    },
+                    }
                     _ => {
                         // TODO: query handler
                     }
                 }
-            },
+            }
             Some(Err(_)) | None => {
                 break;
-            },
+            }
         }
     }
 }
--- src/api/auth.rs
+++ src/api/auth.rs
@@ -1,9 +1,14 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
 
 use async_trait::async_trait;
-use futures::sink::Sink;
+use futures::sink::{Sink, SinkExt};
+use futures::stream;
+use rand;
 
-use super::ClientInfo;
+use super::{ClientInfo, PgWireConnectionState};
+use crate::messages::response::{ReadyForQuery, READY_STATUS_IDLE};
+use crate::messages::startup::{Authentication, BackendKeyData, ParameterStatus, Startup};
 use crate::messages::PgWireMessage;
 
 // Alternative design: pass PgWireMessage into the trait and allow the
@@ -11,11 +16,56 @@ use crate::messages::PgWireMessage;
 // support for other auth type like sasl.
 
 #[async_trait]
-pub trait Authenticator {
-    // TODO: move connection state into ClientInfo
-    async fn on_startup<C>(&self, client: &mut C, message: &PgWireMessage) -> Result<(), std::io::Error>
+pub trait StartupHandler {
+    async fn on_startup<C>(
+        &self,
+        client: &mut C,
+        message: &PgWireMessage,
+    ) -> Result<(), std::io::Error>
     where
-        C: ClientInfo + Sink<PgWireMessage> + Unpin + Debug + Send, C::Error: Debug;
+        C: ClientInfo + Sink<PgWireMessage> + Unpin + Send,
+        C::Error: Debug;
+
+    fn handle_startup_parameters<C>(&self, client: &mut C, startup_message: &Startup)
+    where
+        C: ClientInfo + Sink<PgWireMessage> + Unpin + Send,
+        C::Error: Debug,
+    {
+        client.metadata_mut().extend(
+            startup_message
+                .parameters()
+                .iter()
+                .map(|(k, v)| (k.to_owned(), v.to_owned())),
+        );
+    }
+
+    async fn finish_authentication<C>(&self, client: &mut C)
+    where
+        C: ClientInfo + Sink<PgWireMessage> + Unpin + Send,
+        C::Error: Debug,
+    {
+        client.set_state(PgWireConnectionState::ReadyForQuery);
+        let mut messages = Vec::new();
+        messages.push(PgWireMessage::Authentication(Authentication::Ok));
+        for (k, v) in self.server_parameters(client) {
+            messages.push(PgWireMessage::ParameterStatus(ParameterStatus::new(k, v)));
+        }
+
+        messages.push(PgWireMessage::BackendKeyData(BackendKeyData::new(
+            std::process::id() as i32,
+            rand::random::<i32>(),
+        )));
+        messages.push(PgWireMessage::ReadyForQuery(ReadyForQuery::new(
+            READY_STATUS_IDLE,
+        )));
+        let mut message_stream = stream::iter(messages.into_iter().map(Ok));
+        client.send_all(&mut message_stream).await.unwrap();
+    }
+
+    fn server_parameters<C>(&self, client: &C) -> HashMap<String, String>
+    where
+        C: ClientInfo + Sink<PgWireMessage> + Unpin + Send,
+        C::Error: Debug;
 }
 
 // TODO: password handler
