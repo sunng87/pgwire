c707d5e74f3888ee0056a90991d6f9bb84ad1c1a
Ning Sun <sunng@protonmail.com>
2022-07-26 23:11:04 +0800

    feat: add content to portal and stmt

--- src/api/portal.rs
+++ src/api/portal.rs
@@ -1,2 +1,9 @@
-#[derive(Debug, Default)]
-pub struct Portal {}
+use crate::types::Data;
+
+#[derive(Debug, Default, new, Getters, Setters)]
+#[getset(get = "pub", set = "pub", get_mut = "pub")]
+pub struct Portal {
+    id: String,
+    stmt_id: String,
+    parameters: Vec<Data>,
+}
--- src/api/stmt.rs
+++ src/api/stmt.rs
@@ -1,4 +1,10 @@
-#[derive(Debug, Default)]
+#[derive(Debug, Default, new, Getters, Setters)]
+#[getset(get = "pub", set = "pub", get_mut = "pub")]
 pub struct Statement {
+    // name of the statement, empty string for unnamed
+    id: String,
+    // query statement
     statement: String,
+    // type ids of query parameters
+    type_oids: Vec<i32>,
 }
