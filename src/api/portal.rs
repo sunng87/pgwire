fd940ac79bc7aabebe33410cd945bf7e9fd737ca
Ning Sun <sunng@protonmail.com>
2022-08-07 20:54:21 +0800

    feat: implement convertion from bind to portal

--- src/api/portal.rs
+++ src/api/portal.rs
@@ -1,8 +1,13 @@
-use crate::{messages::extendedquery::Bind, types::Data};
+use itertools;
 
-use super::DEFAULT_NAME;
+use crate::{
+    messages::{data::FORMAT_CODE_TEXT, extendedquery::Bind},
+    types::Data,
+};
 
-#[derive(Debug, Default, new, Getters, Setters)]
+use super::{stmt::Statement, DEFAULT_NAME};
+
+#[derive(Debug, Default, Getters, Setters)]
 #[getset(get = "pub", set = "pub", get_mut = "pub")]
 pub struct Portal {
     id: String,
@@ -10,8 +15,8 @@ pub struct Portal {
     parameters: Vec<Data>,
 }
 
-impl From<&Bind> for Portal {
-    fn from(bind: &Bind) -> Portal {
+impl Portal {
+    fn new(bind: &Bind, statement: &Statement) -> Portal {
         Portal {
             id: bind
                 .portal_name()
@@ -21,8 +26,46 @@ impl From<&Bind> for Portal {
                 .statement_name()
                 .clone()
                 .unwrap_or_else(|| DEFAULT_NAME.to_owned()),
-            // FIXME:
-            parameters: bind.parameters().clone(),
+            parameters: parse_parameters(bind, statement),
         }
+    }
+}
+
+fn parse_parameters(bind: &Bind, statement: &Statement) -> Vec<Data> {
+    let bind_format_codes = bind.parameter_format_codes();
+    let unified_format_code = if bind_format_codes.is_empty() {
+        Some(FORMAT_CODE_TEXT)
+    } else if bind_format_codes.len() == 1 {
+        Some(bind_format_codes.get(0).cloned().unwrap())
+    } else {
+        None
+    };
+
+    if let Some(format_code) = unified_format_code {
+        itertools::zip(bind.parameters(), statement.type_oids())
+            .map(|(value, type_oid)| parse_value(format_code, *type_oid, value))
+            .collect::<Vec<Data>>()
+    } else {
+        itertools::izip!(
+            bind.parameters(),
+            bind.parameter_format_codes(),
+            statement.type_oids(),
+        )
+        .map(|(value, format_code, type_oid)| parse_value(*format_code, *type_oid, value))
+        .collect::<Vec<Data>>()
+    }
+}
+
+fn parse_value(format_code: i16, type_oid: i32, value: &Option<Vec<u8>>) -> Data {
+    if let Some(ref value) = value {
+        if format_code == FORMAT_CODE_TEXT {
+            // text
+            Data::from_text(type_oid, String::from_utf8_lossy(value).as_ref())
+        } else {
+            // binary
+            Data::from_bytes(type_oid, value)
+        }
+    } else {
+        Data::Null
     }
 }
