fcd3c3b3ce00c50cd5adca1d1d8e3bcba38e6dd9
Ning Sun <sunng@protonmail.com>
2022-12-26 23:01:08 +0800

    feat: wip scram auth handler

--- src/api/auth/scram.rs
+++ src/api/auth/scram.rs
@@ -15,11 +15,36 @@ use crate::{
 use super::{PasswordVerifier, ServerParameterProvider, StartupHandler};
 
 #[derive(new)]
-pub struct SASLScramAuthStartupHandler<V, P> {
-    verifier: V,
+pub struct SASLScramAuthStartupHandler<A, P> {
+    auth_db: Arc<A>,
     parameter_provider: P,
     state: AtomicU8,
+
+    /// cached salted_password
+    salted_password: Option<Vec<u8>>,
+    /// client-first-message-bare
+    client_first: Option<String>,
+    /// server-first-message
+    server_first: Option<String>,
+    /// client-final-message-without-proof
+    client_final: Option<String>,
 }
+
+#[async_trait]
+pub trait AuthDB {
+    /// Fetch password and add salt, this step is defined in RFC5802
+    ///
+    /// ```text
+    /// SaltedPassword  := Hi(Normalize(password), salt, i)
+    /// ```
+    ///
+    /// The implementation should first retrieve password from its storage and
+    /// compute it into SaltedPassword
+    async fn get_salted_password(&self, username: &str, salt: &[u8], iterations: usize) -> Vec<u8>;
+}
+
+///
+pub fn salt_password(password: &[u8], salt: &[u8], iters: usize) -> Vec<u8> {}
 
 #[async_trait]
 impl<V: PasswordVerifier, P: ServerParameterProvider> StartupHandler
@@ -50,6 +75,8 @@ impl<V: PasswordVerifier, P: ServerParameterProvider> StartupHandler
                     let resp = msg.into_sasl_initial_response()?;
                     let method = resp.auth_method();
                     let client_first = resp.data();
+
+                    self.state.fetch_add(Ordering::Relaxed, 1);
                 } else {
                     // second response, client_final
                     let resp = msg.into_sasl_response()?;
