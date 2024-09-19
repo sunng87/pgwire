686b86b895425f7627ce7a662c34edf03c2e6ff4
Hadi Zamani <hadi.zamani@hamravesh.com>
2024-09-19 17:33:25 +0330

    test: show result more specificly

--- tests-integration/go/client.go
+++ tests-integration/go/client.go
@@ -1,29 +1,39 @@
 package main
 
 import (
-  "log"
-  "database/sql"
-  _"github.com/lib/pq"
+    "log"
+    "database/sql"
+    _ "github.com/lib/pq"
 )
-func main() {
-  conninfo := "host=127.0.0.1 port=5432 user=tom password=pencil dbname=localdb"
-  db, err := sql.Open("postgres", conninfo)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer db.Close()
-  
-  _, err = db.Exec("INSERT INTO testtable VALUES (1)")
-  if err != nil {
-    log.Fatal(err)
-  }
-  
-  rows, err := db.Query("SELECT * FROM testtable")
-  if err != nil {
-    log.Fatal(err)
-  }
 
-  for rows.Next() {
-    log.Println(rows)
-  }
+type result struct {
+    id int
+    name string
+    date string
+    isOk bool
+}
+
+func main() {
+    conninfo := "host=127.0.0.1 port=5432 user=tom password=pencil dbname=localdb"
+    db, err := sql.Open("postgres", conninfo)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    _, err = db.Exec("INSERT INTO testtable VALUES (1)")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    rows, err := db.Query("SELECT * FROM testtable")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for rows.Next() {
+        var r result
+        rows.Scan( & r.id, & r.name, & r.date, & r.isOk)
+        log.Printf("%#v", r)
+    }
 }
