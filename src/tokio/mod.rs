deabbe516afd81aa11ef9c952c01ff94380d4788
Philip Dubé <philip@peerdb.io>
2024-12-13 16:06:50 +0000

    tls improvements
    reexport tokio_rustls since TlsAcceptor type must come from same version
    
    breaking change: when tls not enabled still require None to be passed,
    thus code written without tls feature won't be broken when tls enabled

--- src/tokio/mod.rs
+++ src/tokio/mod.rs
@@ -1,3 +1,10 @@
 mod server;
 
 pub use server::process_socket;
+#[cfg(any(feature = "_ring", feature = "_aws-lc-rs"))]
+pub use tokio_rustls;
+#[cfg(any(feature = "_ring", feature = "_aws-lc-rs"))]
+pub type TlsAcceptor = tokio_rustls::TlsAcceptor;
+
+#[cfg(not(any(feature = "_ring", feature = "_aws-lc-rs")))]
+pub enum TlsAcceptor {}
