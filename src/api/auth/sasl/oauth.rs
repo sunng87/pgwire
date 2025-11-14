26aee4a74f1ca00edafd821af902da831c9e87af
Baasit <baasisek01@gmail.com>
2025-11-14 20:50:40 +0100

    chore(server): #298 added basic data structs for implementation

--- src/api/auth/sasl/oauth.rs
+++ src/api/auth/sasl/oauth.rs
@@ -1,4 +1,7 @@
-use std::{fmt::Debug, sync::Arc};
+// ref: https://github.com/postgres/postgres/blob/e7ccb247b38fff342c13aa7bdf61ce5ab45b2a85/src/backend/libpq/auth-oauth
+use std::{collections::HashMap, fmt::Debug, sync::Arc};
+
+use async_trait::async_trait;
 
 use crate::{
     api::{auth::sasl::SASLState, ClientInfo},
@@ -7,21 +10,59 @@ use crate::{
 };
 
 #[derive(Debug)]
-// TODO: find a way to get the issuer and scope, i think it'll be gotten from the client?
-// as per: https://github.com/postgres/postgres/blob/e7ccb247b38fff342c13aa7bdf61ce5ab45b2a85/src/backend/libpq/auth-oauth.c#L102
 pub struct Oauth {
     pub issuer: String,
     pub scope: String,
     pub validator: Arc<dyn OauthValidator>,
+    /// Whether to skip user mapping (validator is authoritative)
+    pub skip_usermap: bool,
 }
 
-// TODO: move this out
-pub trait OauthValidator: Send + Sync + Debug {}
+/// Constants seen in an OAUTHBEARER client initial response.
+///
+/// required header scheme (case-insensitive!)
+const BEARER_SCHEME: &str = "Bearer ";
+/// key containing the Authorization header
+const AUTH_KEY: &str = "auth";
+/// separator byte for key/value pairs
+const KVSEP: u8 = 0x01;
+
+/// the result of validating a token from a validator module (in this case, validator trait)
+#[derive(Debug, Clone)]
+pub struct ValidatorModuleResult {
+    /// whether the token is authorized for the requested access
+    pub authorized: bool,
+    pub authn_id: Option<String>,
+    /// optional: additional claims or metadata for state
+    pub metadata: Option<HashMap<String, String>>,
+}
+
+/// validate a bearer tokjn for a specific user
+#[async_trait]
+pub trait OauthValidator: Send + Sync + Debug {
+    async fn validate(
+        &self,
+        token: &str,
+        username: &str,
+        issuer: &str,
+        required_scopes: &str,
+    ) -> PgWireResult<ValidatorModuleResult>;
+}
 
 impl Oauth {
     /// initialize the oauth context.
-    pub fn init() -> Self {
-        todo!()
+    pub fn new(issuer: String, scope: String, validator: Arc<dyn OauthValidator>) -> Self {
+        Self {
+            issuer,
+            scope,
+            validator,
+            skip_usermap: false,
+        }
+    }
+
+    pub fn with_skip_usermapping(mut self, skip: bool) -> Self {
+        self.skip_usermap = skip;
+        self
     }
 
     pub async fn process_oauth_message<C>(
