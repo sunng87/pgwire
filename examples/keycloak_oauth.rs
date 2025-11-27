936636127d0c0065d19c326d104004731ec7ea3b
Baasit <baasisek01@gmail.com>
2025-11-27 22:41:18 +0100

    chore(server): #298 keycloak example setup

--- examples/keycloak_oauth.rs
+++ examples/keycloak_oauth.rs
@@ -1,7 +1,8 @@
-/// This example shows how to use pgwire with OAuth.
+/// This example shows how to use pgwire with Keycloak OAuth.
 /// To connect with psql:
 /// 1. Install libq-oauth: sudo apt-get install libpq-oauth
-/// 2. Execute: psql "postgres://postgres@localhost:5432/db?oauth_issuer=https://auth.example.com&oauth_client_id=my-app-client-id"
+/// 2. Setup keycloak. check this for more details: https://habr.com/en/companies/tantor/articles/959776/
+/// 2. Execute: psql "postgres://postgres@localhost:5432/postgres?oauth_issuer=http://localhost:8080/realms/postgres-realm&oauth_client_id=postgres-client&oauth_client_secret=<my-client-secret>"
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, Error as IOError, ErrorKind};
@@ -12,7 +13,9 @@ use async_trait::async_trait;
 use pgwire::api::auth::sasl::oauth::{Oauth, OauthValidator, ValidatorModuleResult};
 use rustls_pemfile::{certs, pkcs8_private_keys};
 use rustls_pki_types::{CertificateDer, PrivateKeyDer};
+use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
+use tokio::sync::RwLock;
 use tokio_rustls::rustls::ServerConfig;
 use tokio_rustls::TlsAcceptor;
 
@@ -26,30 +29,83 @@ pub fn random_salt() -> Vec<u8> {
     Vec::from(rand::random::<[u8; 10]>())
 }
 
-// TODO: change to JSON web token validator, this is just for validating the current code
-#[derive(Debug)]
-struct SimpleTokenValidator {
-    valid_tokens: HashMap<String, String>,
+#[derive(Debug, Serialize, Deserialize, Default)]
+struct RealmAccess {
+    #[serde(default)]
+    roles: Vec<String>,
 }
 
-impl SimpleTokenValidator {
-    pub fn new() -> Self {
-        let mut valid_tokens = HashMap::new();
-        valid_tokens.insert(
-            "secret_token_123".to_string(),
-            "user@example.com".to_string(),
-        );
-        valid_tokens.insert(
-            "admin_token_456".to_string(),
-            "admin@example.com".to_string(),
-        );
+#[derive(Debug, Serialiaze, Deserialize)]
+struct KeyCloakClaims {
+    sub: String,
+    scope: Option<String>,
+    #[serde(default)]
+    realm_access: RealmAccess,
+    preferred_username: Option<String>,
+    email: Option<String>,
+    exp: usize,
+    iat: usize,
+    iss: usize,
+}
 
-        Self { valid_tokens }
+/// ODIC discovery doc
+#[derive(Debug, Deserialize)]
+struct OidcDiscovery {
+    issuer: String,
+    jwks_uri: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Jwk {
+    kid: String,
+    kty: String,
+    #[serde(rename = "use")]
+    key_use: Option<String>,
+    // modulus for the rsa algo
+    n: String,
+    // exponent for yhe rsa algo
+    e: String,
+    alg: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Jwks {
+    keys: Vec<Jwk>,
+}
+
+#[derive(Debug, Clone)]
+struct KeyCloakValidator {
+    issuer: String,
+    client: reqwest::Client,
+    jwks_cache: Arc<RwLock<HashMap<String, String>>>,
+}
+
+impl KeyCloakValidator {
+    pub fn new(issuer: String) -> Self {
+        Self {
+            issuer,
+            client: reqwest::Client::new(),
+            jwks_cache: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
+
+    async fn fetch_oidc_discovery(&self) -> Result<OidcDiscovery, Box<dyn std::error::Error>> {
+        let discovery_url = format!("{}/.well-known/openid-configuration", self.issuer);
+        let response = self.client.get(&discovery_url).send().await?;
+        let discovery: OidcDiscovery = response.json().await?;
+        Ok(discovery)
+    }
+
+    async fn fetch_jwks(&self) -> Result<Jwks, Box<dyn std::error::Error>> {
+        let discovery = self.fetch_oidc_discovery().await?;
+        let response = self.client.get(&discovery.jwks_uri).send().await?;
+        let jwks: Jwks = response.json().await?;
+        Ok(jwks)
     }
 }
 
 #[async_trait]
-impl OauthValidator for SimpleTokenValidator {
+impl OauthValidator for KeyCloakValidator {
     async fn validate(
         &self,
         token: &str,
@@ -57,23 +113,7 @@ impl OauthValidator for SimpleTokenValidator {
         issuer: &str,
         required_scopes: &str,
     ) -> PgWireResult<ValidatorModuleResult> {
-        println!("Validating token for user: {}", username);
-        println!("Expected issuer: {}", issuer);
-        println!("Required scopes: {}", required_scopes);
-
-        if let Some(authenticated_user) = self.valid_tokens.get(token) {
-            Ok(ValidatorModuleResult {
-                authorized: true,
-                authn_id: Some(authenticated_user.clone()),
-                metadata: None,
-            })
-        } else {
-            Ok(ValidatorModuleResult {
-                authorized: false,
-                authn_id: None,
-                metadata: None,
-            })
-        }
+        todo!()
     }
 }
 
