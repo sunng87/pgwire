c37cd79a603934b06f7b782b20e08c8b6973022e
Baasit <baasisek01@gmail.com>
2025-12-08 23:10:18 +0100

    refactor(server): #362 added new oauth/oidc feature gate

--- src/api/auth/mod.rs
+++ src/api/auth/mod.rs
@@ -352,5 +352,5 @@ pub mod md5pass;
 pub mod noop;
 #[cfg(any(feature = "_aws-lc-rs", feature = "_ring"))]
 pub mod sasl;
-#[cfg(any(feature = "_aws-lc-rs", feature = "_ring"))]
-pub mod simple_oauth;
+#[cfg(any(feature = "_aws-lc-rs", feature = "simple-oidc-validator"))]
+pub mod simple_oidc_validator;
