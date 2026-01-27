90ca41f95438bff36ef7b27a350c7fd4eafe4149
Ning Sun <sunng@protonmail.com>
2026-01-27 18:20:23 +0800

    test: add a libpq test client in integration tests (#390)
    * test: add a libpq test client in integration tests
    
    * test: use nix environment to run ci
    
    * fix: attemp to nix environment
    
    * ci: update how we setup nix
    
    * chore: try to include openssl in flake
    
    * chore: try to figure out the minimal configuration for openssl
    
    * chore: revert changes

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
           llvmPackages.libclang
           sqlite.dev
           sqlite.out
-          openssl.out
+          openssl
           libpq.dev
           libpq.out
         ];
@@ -56,6 +56,8 @@
             openssh
             jbang
             jdk_headless
+            gnumake
+            openssl
           ];
 
 
@@ -66,6 +68,13 @@
             export CXX=clang++
             export SQLITE3_LIB_DIR="${pkgs.sqlite.dev}/lib"
             export SQLITE3_INCLUDE_DIR="${pkgs.sqlite.dev}/include"
+
+            export OPENSSL_DIR="${pkgs.openssl.dev}"
+            export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
+            export OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include"
+
+            export LD_LIBRARY_PATH="${pkgs.openssl.out}/lib:$LD_LIBRARY_PATH"
+            export LIBRARY_PATH="${pkgs.openssl.out}/lib:$LIBRARY_PATH"
           '';
         };
       });
