1ccf8d5b29703b2901da244f525dc5e06a5466eb
Ning Sun <sunning@greptime.com>
2026-02-12 16:22:13 +0800

    chore: update client demo and certificates

--- examples/client.rs
+++ examples/client.rs
@@ -1,21 +1,45 @@
+use std::fs::File;
+use std::io::{BufReader, Error as IOError};
 use std::sync::Arc;
 
 use pgwire::api::client::auth::DefaultStartupHandler;
 use pgwire::api::client::query::DefaultSimpleQueryHandler;
 use pgwire::api::client::ClientInfo;
 use pgwire::tokio::client::PgWireClient;
+use rustls_pki_types::CertificateDer;
+use tokio_rustls::rustls::{ClientConfig, RootCertStore};
+use tokio_rustls::TlsConnector;
+
+fn setup_tls() -> Result<Arc<ClientConfig>, IOError> {
+    let mut roots = RootCertStore::empty();
+
+    let cert = rustls_pemfile::certs(&mut BufReader::new(File::open("examples/ssl/server.crt")?))
+        .collect::<Result<Vec<CertificateDer>, _>>()?;
+
+    roots.add_parsable_certificates(cert);
+
+    let config = ClientConfig::builder()
+        .with_root_certificates(roots)
+        .with_no_client_auth();
+
+    Ok(Arc::new(config))
+}
 
 #[tokio::main]
 pub async fn main() {
     let config = Arc::new(
-        "host=127.0.0.1 port=5432 user=pgwire dbname=demo password=pencil"
+        "host=127.0.0.1 port=5433 user=pgwire dbname=demo password=pencil sslmode=prefer"
             .parse()
             .unwrap(),
     );
     let startup_handler = DefaultStartupHandler::new();
-    let mut client = PgWireClient::connect(config, startup_handler, None)
-        .await
-        .unwrap();
+    let mut client = PgWireClient::connect(
+        config,
+        startup_handler,
+        Some(TlsConnector::from(setup_tls().unwrap())),
+    )
+    .await
+    .unwrap();
 
     println!("{:?}", client.server_parameters());
 
